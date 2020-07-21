53b998d5554da71b09716b0b8339c710da910bad
Xux <1102828502@qq.com>
2020-07-21 10:19:23 +0800

    第一次提交

---
+++ WebApplication1/Controllers/ContentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
---
+++ WebApplication1/Models/ContentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ContentViewModel
+    {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
